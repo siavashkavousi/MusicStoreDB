72d0ad601b3ff3c1610935ca6f2d5b8f2fabed25
siavash <siavash.kavousi.sk@gmail.com>
2016-05-14 03:17:50 +0430

    Adds track informations

--- schema_insertions.sql
+++ schema_insertions.sql
@@ -1,3 +1,6 @@
-INSERT INTO album (name) VALUES ('Radioactive'), ('Safe & Sound'), ('Dreamlife'),
-  ('Within'), ('Fragments Found');
+INSERT INTO album (name) VALUES ('Tolou'), ('Once In A Red Moon'), ('The Piano Guys');
+
+INSERT INTO track (album_id, name, duration) VALUES (1, 'Naajee', '430'),
+  (1, 'Molaghat', '530'), (2, 'Gates of Dawn', '429'), (3, 'A Thousand Years', '436'),
+  (3, 'Let It Go', '402'), (3, 'Summer Jam', '354');
 
