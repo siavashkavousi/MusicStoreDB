4713817962c1aae7e1a3973269ea9e8f56594f72
siavash <siavash.kavousi.sk@gmail.com>
2016-05-14 03:28:37 +0430

    Adds some tuples to tables

--- schema_insertions.sql
+++ schema_insertions.sql
@@ -4,3 +4,12 @@ INSERT INTO track (album_id, name, duration) VALUES (1, 'Naajee', '430'),
   (1, 'Molaghat', '530'), (2, 'Gates of Dawn', '429'), (3, 'A Thousand Years', '436'),
   (3, 'Let It Go', '402'), (3, 'Summer Jam', '354');
 
+INSERT INTO artist (name) VALUES ('Moein'), ('The Piano Guys'), ('Secret Garden');
+
+INSERT INTO track_artist (track_id, artist_id) VALUES (1, 1), (2, 1), (3, 3), (4, 2), (5, 2), (6, 2);
+
+INSERT INTO customer (name, age) VALUES ('Siavash', 22), ('Ali', 20), ('Ghasemi', 21), ('Goolakh', 33);
+
+INSERT INTO genre (type) VALUES ('Pop'), ('Jazz'), ('Instrumental'), ('Rock'), ('Hip Hop');
+
+INSERT INTO comment (track_id, customer_id, content) VALUES (2, 1, 'Alie in ahang'), (6, 2, 'Bass khoobi dare');
