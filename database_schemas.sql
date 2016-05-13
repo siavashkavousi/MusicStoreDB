a3db260bc4493e842042669690ab1ab12167afe4
siavash <siavash.kavousi.sk@gmail.com>
2016-05-14 02:21:45 +0430

    Finishes database schemas

--- database_schemas.sql
+++ database_schemas.sql
@@ -16,9 +16,47 @@ CREATE TABLE artist (
   name      VARCHAR(32) NOT NULL
 );
 
-CREATE TABLE artist_track (
+CREATE TABLE track_artist (
   track_id  INT,
   artist_id INT NOT NULL,
-  FOREIGN KEY (track_id) REFERENCES track(track_id),
-  FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
+  FOREIGN KEY (track_id) REFERENCES track (track_id),
+  FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
+);
+
+CREATE TABLE comment (
+  comment_id  INT AUTO_INCREMENT PRIMARY KEY,
+  track_id    INT          NOT NULL,
+  customer_id INT          NOT NULL,
+  content     VARCHAR(255) NOT NULL,
+  FOREIGN KEY (track_id) REFERENCES track (track_id),
+  FOREIGN KEY (customer_id) REFERENCES customer (customer_id)
+);
+
+CREATE TABLE customer (
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  name        VARCHAR(32) NOT NULL,
+  age         INT
+);
+
+CREATE TABLE genre (
+  type VARCHAR(20) PRIMARY KEY
+);
+
+CREATE TABLE track_genre (
+  track_id INT         NOT NULL,
+  type     VARCHAR(20) NOT NULL,
+  FOREIGN KEY (track_id) REFERENCES track (track_id),
+  FOREIGN KEY (type) REFERENCES genre (type)
+);
+
+CREATE TABLE 'order' (
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  price    INT
+);
+
+CREATE TABLE track_order (
+  track_id INT NOT NULL,
+  order_id INT,
+  FOREIGN KEY (track_id) REFERENCES track (track_id),
+  FOREIGN KEY (order_id) REFERENCES `order` (order_id)
 );
