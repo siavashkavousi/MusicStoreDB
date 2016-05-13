5175d6062571d394057398ccdad449135e732f37
siavash <siavash.kavousi.sk@gmail.com>
2016-05-14 02:53:49 +0430

    Adds some values to album

---
+++ schema_insertions.sql
@@ -0,0 +1,5 @@
+INSERT album VALUES ('Radioactive');
+INSERT album VALUES ('Safe & Sound');
+INSERT album VALUES ('Dreamlife');
+INSERT album VALUES ('Within');
+INSERT album VALUES ('Fragments Found');
