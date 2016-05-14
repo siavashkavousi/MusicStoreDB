1f604dd0677c10a8e89d14265341926bbeaef410
siavash <siavash.kavousi.sk@gmail.com>
2016-05-14 14:51:44 +0430

    Adds second query

--- queries.sql
+++ queries.sql
@@ -15,5 +15,18 @@ FROM track
 WHERE type = 'Pop'
 GROUP BY album_id
 HAVING sum(price)
-ORDER BY sum(price)
+ORDER BY sum(price);
 
+# Query #2
+SELECT
+  track_id,
+  track.name,
+  avg(star)
+FROM (track
+  NATURAL JOIN track_genre
+  NATURAL JOIN genre) JOIN comment USING (track_id)
+  JOIN customer USING (customer_id)
+WHERE type = 'Rock'
+GROUP BY track_id, track.name
+HAVING avg(star)
+ORDER BY avg(star) DESC;
